9e058cb84e91a2cc3688a727050938940528c789
Dave Smith <dsmith@hirevue.com>
2012-10-26 11:51:53 -0600

    Tabs to spaces. Tabs are evil

--- static/demo5.js
+++ static/demo5.js
@@ -2,30 +2,30 @@
 
 var BankAccount = function(acctName) {
 
-	var _acctName = acctName
-	var _myCash = 0;
+    var _acctName = acctName
+    var _myCash = 0;
 
-	function deposit(amount) {
-		_log("Depositing " + amount + " into account " + _acctName);
-		_myCash += amount;
-	}
+    function deposit(amount) {
+        _log("Depositing " + amount + " into account " + _acctName);
+        _myCash += amount;
+    }
 
-	function withdraw(amount) {
-		_log("Withdrawing " + amount + " from account " + _acctName);
-		_myCash -= amount;
-	}
+    function withdraw(amount) {
+        _log("Withdrawing " + amount + " from account " + _acctName);
+        _myCash -= amount;
+    }
 
-	function balance() {
-		return _myCash;
-	}
+    function balance() {
+        return _myCash;
+    }
 
-	function _log(msg) {
-		console.log(msg);
-	}
+    function _log(msg) {
+        console.log(msg);
+    }
 
-	return {
-		deposit  : deposit,
-		withdraw : withdraw,
-		balance  : balance
-	}
+    return {
+        deposit  : deposit,
+        withdraw : withdraw,
+        balance  : balance
+    }
 }
